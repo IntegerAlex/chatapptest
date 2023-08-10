0e396489b102e78329d30370bab0d58c71da1beb
IntegerAlex <inquiry.akshatkotpalliwar@gmail.com>
2023-08-10 22:26:20 +0530

    wtf
    i dunno i dunno

--- public/index.js
+++ public/index.js
@@ -53,7 +53,7 @@ io.on('connection', (socket) => {
 
   const admin = require('firebase-admin');
 
-  const serviceAccount = require('./hackster-82fa2-firebase-adminsdk-139hh-cba3809411.json'); // Replace with your actual service account key
+  const serviceAccount = require('../hackster-82fa2-firebase-adminsdk-139hh-cba3809411.json'); // Replace with your actual service account key
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: 'https://hackster-82fa2.firebaseio.com/' // Replace with your actual Firebase Database URL
